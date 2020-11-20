c20406c754a6de019db9cbcd0cf928c521886c0e
offdutybyblo <offdutybyblo@gmail.com>
2020-11-20 17:41:01 +0900

    wip: flexible mobile css

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// slic slider module
+
 $(document).ready(function(){
   $('.slider__part').slick({
       slidesToShow: 1,
@@ -7,5 +9,24 @@ $(document).ready(function(){
       dots: true,
       arrows: true,
       adaptiveHeight: true,
+      responsive: [
+        {
+          breakpoint: 420,
+          settings: {
+            width:420
+          }
+        }
+      ]
   });
 });
+
+
+var state = true;
+const aaaa = document.getElementsByClassName('hidden__menubar')[0];
+console.log(aaaa);
+
+aaaa.addEventListener("click", function(){
+  console.log('hi')
+  state = !state
+  console.log(state)
+})
