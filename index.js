638a70bc857b3f7f5675c1c05f3f0f5e558d4715
offdutybyblo <offdutybyblo@gmail.com>
2020-11-23 15:48:15 +0900

    responsive/modal : complete

--- index.js
+++ index.js
@@ -34,12 +34,38 @@ $(document).ready(function(){
 });
 
 
-// var state = true;
-// const aaaa = document.getElementsByClassName('hidden__menubar')[0];
-// console.log(aaaa);
+// Modal Logic
 
-// aaaa.addEventListener("click", function(){
-//   console.log('hi')
-//   state = !state
-//   console.log(state)
+// document.getElementsByClassName("mobile__menubar").addEventListener('click', function() {
+//   console.log('hi');
 // })
+const modal = document.getElementById('modal__menu')
+const menubar = document.getElementById('mobile__menubar');
+const remove = document.getElementById('remove__modal');
+const body  =  document.body
+
+menubar.addEventListener('click', function(){
+    modal.style.visibility = 'visible';
+    modal.style.left = 0;
+    modal.style.top = 0;
+    modal.style.opacity = 1;
+    modal.style.transition = 'linear 900ms';
+
+    body.style.overflow = 'hidden';
+    body.style.position = 'fixed';
+    body.style.width = '100%';
+    body.style.height = '100%';
+    // modal.id = "plusModal"
+    // body.id = "plusBody"
+})
+
+remove.addEventListener('click', function(){
+  modal.style.visibility = 'hidden';
+  modal.style.left = '1100px';
+  modal.style.top = 0;
+  modal.style.opacity = 0;
+  body.style.overflow = 'visible';
+  body.style.position = 'static';
+  body.style.width = '100%';
+  body.style.height = '100%';
+})
